c18ecf47e982e97c3cf3c728313709b466e59dbe
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-07-25 17:22:27 -0300

    :tada: initial commit

---
+++ src/data/constants/products.ts
@@ -0,0 +1,46 @@
+import Product from "../model/Product";
+
+const products: Product[] = [
+    {
+        id: 1,
+        name: "KeyBoard Mechanical",
+        price: 231.9,
+        description: "This high-performance Machenike keyboard allows you to enjoy unlimited hours of gaming. It was specially developed so you can express your skills and style. Enhance your gaming experience, whether you're an amateur or an expert, and take your plays to the next level.",
+        image: "https://images.unsplash.com/photo-1712396901531-605f06a423aa?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: 2,
+        name: "Mouse Logitech",
+        price: 185.9,
+        description: "Supreme comfort and durability, designed for gaming with an easy-to-grip and control shape, high-quality materials, lightweight construction (87 g), rubberized sides, and a removable 10 g weight.",
+        image: "https://images.unsplash.com/photo-1618499890638-3a0dd4b278b7?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: 3,
+        name: "Mousepad",
+        price: 69.9,
+        description: "With its minimalist design and clean white color, it fits perfectly in any setting, whether in the office, home office, or gaming setup. It is easy to clean and maintains its elegant appearance even after daily use.",
+        image: "https://images.unsplash.com/photo-1631098983935-5363b8e50edb?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: 4,
+        name: "Laptop",
+        price: 4502.9,
+        description: "Experience the perfect blend of power and portability with our Ultra-Slim Laptop. Designed for both professionals and casual users, this laptop features a sleek, lightweight design that makes it easy to carry and ideal for on-the-go computing.",
+        image: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: 5,
+        name: "Computer",
+        price: 7000,
+        description: "This gaming PC features a top-of-the-line processor and graphics card to ensure ultra-smooth gameplay and stunning visuals. With 32GB of high-speed RAM, you’ll experience fast load times and efficient multitasking. The combination of a 1TB NVMe SSD and a 2TB HDD provides ample storage for your games and media, with rapid access times.",
+        image: "https://images.unsplash.com/photo-1660855551550-2696677aaf28?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: 6,
+        name: "27-Inch 240Hz Gaming Monitor",
+        price: 4502.9,
+        description: "Elevate your gaming experience with the 27-Inch 240Hz Gaming Monitor, designed for those who demand unparalleled performance and fluidity. This monitor boasts a stunning 27-inch display with a high-refresh-rate of 240Hz, delivering exceptionally smooth and responsive visuals that keep you ahead of the competition.",
+        image: "https://images.unsplash.com/photo-1614624532983-4ce03382d63d?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+]
